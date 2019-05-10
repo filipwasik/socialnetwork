ebfb8ead14336c58fe044aefd9c852e01865b09d
Paul Hildebrandt <33395806+paulhldbrndt@users.noreply.github.com>
2019-05-10 18:34:08 +0200

    Update socialnetwork.sql

--- socialnetwork.sql
+++ socialnetwork.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.4
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: May 10, 2019 at 08:29 AM
--- Server version: 10.1.37-MariaDB
--- PHP Version: 5.6.40
+-- Erstellungszeit: 10. Mai 2019 um 18:31
+-- Server-Version: 10.1.38-MariaDB
+-- PHP-Version: 7.1.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,45 +19,44 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `socialnetwork`
+-- Datenbank: `socialnetwork`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Tabellenstruktur für Tabelle `user`
 --
 
 CREATE TABLE `user` (
-  `ID` int(11) NOT NULL,
   `username` varchar(20) NOT NULL,
   `profile` int(11) NOT NULL,
   `contacts` int(11) NOT NULL,
-  `firstname` int(11) NOT NULL,
-  `lastname` int(11) NOT NULL,
-  `date` int(11) NOT NULL,
-  `status` int(11) NOT NULL
+  `firstname` varchar(25) NOT NULL,
+  `lastname` varchar(25) NOT NULL,
+  `birthdate` date NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(25) NOT NULL,
+  `gender` varchar(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='email';
 
 --
--- Indexes for dumped tables
+-- Daten für Tabelle `user`
+--
+
+INSERT INTO `user` (`username`, `profile`, `contacts`, `firstname`, `lastname`, `birthdate`, `status`, `email`, `password`, `gender`) VALUES
+('alebasone', 0, 0, 'ale', 'basone', '1999-12-12', 0, 'ale@gmail.com', '$2y$10$rhoLj.lCQ/LS6k2CH6', 'female');
+
+--
+-- Indizes der exportierten Tabellen
 --
 
 --
--- Indexes for table `user`
+-- Indizes für die Tabelle `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`ID`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  ADD PRIMARY KEY (`username`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
