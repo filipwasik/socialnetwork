d21823abe2b4b7bd65d619f6e5a3f7e8fd5f0626
Paul Hildebrandt <33395806+paulhldbrndt@users.noreply.github.com>
2019-05-10 23:02:15 +0200

    Update header.php

--- header.php
+++ header.php
@@ -17,21 +17,21 @@
 	<nav>
 		<div class="main-wrapper">
 			<ul>
-				<li><a href="">Home</a></li>
+				<li><b><a href="index.php">Social Network</a></li></b>
 			</ul>
 			<div class="nav-login">
 				<?php
-					
+
 					if (isset($_SESSION['u_id'])) {
 						echo '<form action="includes/logout.inc.php" method="POST">
 							<button type="submit" name="submit">Logout</button>
 						</form>';
 					}
-					
+
 					else {
 						echo '<form action="includes/login.inc.php" method="POST">
-							<input type="text" name="uid" placeholder="Username/e-mail">
-							<input type="password" name="pwd" placeholder="password">
+							<input type="text" name="username" placeholder="Username">
+							<input type="password" name="password" placeholder="Password">
 							<button type="submit" name="submit">Login</button>
 						</form>
 						<a href="signup.php">Sign up</a>';
