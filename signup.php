bbf9c9576d27239571ba57ecd85e77f9865166a5
Paul Hildebrandt <33395806+paulhldbrndt@users.noreply.github.com>
2019-05-10 17:55:44 +0200

    Update signup.php

--- signup.php
+++ signup.php
@@ -6,11 +6,11 @@
 	<div class="main-wrapper">
 		<h2>Signup</h2>
 		<form class="signup-form" action="includes/signup.inc.php" method="POST">
-			<input type="text" name="first" placeholder="Firstname">
-			<input type="text" name="last" placeholder="Lastname">
+			<input type="text" name="firstname" placeholder="Firstname">
+			<input type="text" name="lastname" placeholder="Lastname">
 			<input type="text" name="email" placeholder="E-mail">
-			<input type="text" name="uid" placeholder="Username">
-			<input type="password" name="pwd" placeholder="Password">
+			<input type="text" name="username" placeholder="Username">
+			<input type="password" name="password" placeholder="Password">
 			<button type="submit" name="submit">Sign up</button>
 		</form>
 	</div>
