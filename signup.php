058c961d0536eb761d2c198ef40efab98bcc3e8b
Paul Hildebrandt <33395806+paulhldbrndt@users.noreply.github.com>
2019-05-10 23:01:45 +0200

    Update signup.php

--- signup.php
+++ signup.php
@@ -4,12 +4,12 @@
 
 <section class="main-container">
 	<div class="main-wrapper">
-		<h2>Signup</h2>
+		<h1>Sign up</h1>
 		<form class="signup-form" action="includes/signup.inc.php" method="POST">
 			<input type="text" name="firstname" placeholder="Firstname">
 			<input type="text" name="lastname" placeholder="Lastname">
 			Male <input type="radio" name="gender" value="male">
-  		Female <input type="radio" name="gender" value="female">
+  		Female <input type="radio" class="radio"  name="gender" value="female">
 			<input type="date" name="birthdate" placeholder="Birthdate">
 			<input type="text" name="email" placeholder="E-Mail">
 			<input type="text" name="username" placeholder="Username">
